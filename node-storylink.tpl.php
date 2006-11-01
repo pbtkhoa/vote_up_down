471a4d2b6f1d336c0b447492e8b6de04ccc46bb1
Fredrik Jonsson <frjo@xdeb.org>
2006-11-01 17:40:34 +0000

    This is a big update to the 4.7 version. It adds a Digg style voting widget, integration with the Userpoints module, installation instructions etc. from the head/cvs version. I will now start updating the head/cvs version for Drupal 5.

--- node-storylink.tpl.php
+++ node-storylink.tpl.php
@@ -1,16 +1,14 @@
 
 <!-- start node -->
-<div class="node storylink<?php print ($sticky) ? ' sticky' : ''; ?>">
+<div id="node-<?php print $node->nid; ?>" class="node storylink clear-block<?php print ($sticky) ? ' sticky' : ''; print (!$status) ? ' node-unpublished' : ''; ?>">
 <?php print $picture ?>
-<h2><a href="<?php print $storylink_url ?>"><?php print ($seqid) ? $seqid .'. ' : '' ?><?php print $title ?></a></h2>
+<h2 class="title"><a href="<?php print $storylink_url ?>"><?php print ($seqid) ? $seqid .'. ' : '' ?><?php print $title ?></a></h2>
 <?php print $vote_storylink_via ?>
 <?php print $vote_up_down_widget ?>
-<div class="content">
-<?php print $content ?>
-</div>
-<div class="info"><?php print $submitted ?>
+<div class="content"><?php print $content ?></div>
+<div class="submitted"><?php print $submitted ?>
 <?php if ($terms): ?>
-<span class="terms"> | <?php print t('Category') ?>: <?php print $terms ?></span>
+<span class="terms"> | <?php print t('Tags') ?>: <?php print $terms ?></span>
 <?php endif; ?>
 </div>
 <?php if ($links): ?>
