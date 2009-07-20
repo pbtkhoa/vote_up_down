284cdfbafe4d0a5b4236ecf70f3568423c36e6a4
Pratul Kalia <pratul@pratul.in>
2009-07-20 07:29:00 +0000

    GSoC 2009: Updating various files, added a WIDGETAPI.txt, worked on the alternate/ theme

--- widgets/alternate/widget.tpl.php
+++ widgets/alternate/widget.tpl.php
@@ -5,9 +5,11 @@
  *
  * Alternate widget theme for Vote Up/Down
  */
+$unsigned_points = (int) $points;
 ?>
 <div class="vud-widget-<?php print $widget_theme; ?>">
 <?php if ($class_up) : ?>
-  <span id="vote_up_<?php print $cid; ?>" class="<?php print $class_up; ?>" title="<?php print $title_up; ?>"><?php print $link_up; ?></span>
+  <div id="total-votes-<?php print $cid; ?>" class="<?php print $widget_theme; ?>-votes-display"><?php print $unsigned_points; ?></div>
+  <div id="vote-up-<?php print $cid; ?>" class="<?php print $class_up; ?>" title="<?php print $title_up; ?>"><?php print $link_up; ?></div>
 <?php endif; ?>
 </div>
