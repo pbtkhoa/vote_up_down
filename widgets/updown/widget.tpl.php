4666f4690b32125d2c71c2075f44ba8a9c1e2d47
Pratul Kalia <pratul@pratul.in>
2009-08-16 19:22:05 +0000

    GSoC 2009: Adding upgrade path from UpDown, some other changes.

--- widgets/updown/widget.tpl.php
+++ widgets/updown/widget.tpl.php
@@ -2,14 +2,16 @@
 // $Id$
 
 /**
- * @file widget.tpl.php
+ * @file
+ * widget.tpl.php
  *
  * UpDown widget theme for Vote Up/Down
  */
 $unsignpoints = (int) $points;
+if ($unsignpoints > 1 || $unsignpoints < -1) { $lbl = "votes"; } else { $lbl = "vote"; }
 ?>
 <div class="vud-widget-updown">
-  <div class="updown-score"><span class="updown-current-score" id="total-votes-<?php print $type; ?>-<?php print $cid; ?>"><?php print $unsignpoints; ?></span> votes</div>
+  <div class="updown-score"><span class="updown-current-score" id="total-votes-<?php print $type; ?>-<?php print $cid; ?>"><?php print $unsignpoints; ?></span><?php print $lbl; ?></div>
   <a href="<?php print $link_up; ?>"><div class="<?php print $class_up; ?>" id="updown-up-<?php print $type; ?>-<?php print $cid; ?>" title="Vote up!">+</div></a>
   <a href="<?php print $link_down; ?>"><div class="<?php print $class_down; ?>" id="updown-down-<?php print $type; ?>-<?php print $cid; ?>" title="Vote down!">-</div></a>
 </div>
